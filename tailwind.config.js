d4fd0d7af8e439caa1110ee7055946df5e4c224c
midasreegency <142367691+midasreegency@users.noreply.github.com>
2024-03-25 15:39:58 +0530

    webiste

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+const plugin = require('tailwindcss/plugin')
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    // ...
+    require('@tailwindcss/forms'),
+  ],
+
+}
+
